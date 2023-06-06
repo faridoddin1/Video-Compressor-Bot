0ccbe3a707fe6be960e7fbfff9bbcbb7f4a4d0a2
faridoddin1 <61759070+faridoddin1@users.noreply.github.com>
2023-06-06 17:30:49 +0330

    Update main.py

--- main.py
+++ main.py
@@ -119,5 +119,5 @@ def video(client, message):
 
 
 # infinty polling
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=7860)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
